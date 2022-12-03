0654d88dba458be3c5220b5602dcef56f24c3eb5
Kostya Zelenin <constantine.green80@yandex.ru>
2022-12-03 14:33:22 +0300

    Mesto v2.0 after review

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let profileEditButton = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__name');
-let popupDescriprion = document.querySelector('.popup__description');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupDescriprion = document.querySelector('.popup__input_type_description');
 let popupSubmit = document.querySelector('.popup__submit');
 let popupClose = document.querySelector('.popup__close');
+let popupForm = document.querySelector('.popup__form')
 
-function openPopup(event) {
-    event.preventDefault();
+function openPopup() {
     popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupDescriprion.value = profileDescription.textContent;
 }
 profileEditButton.addEventListener('click', openPopup);
 
-function closePopup(event) {
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 popupClose.addEventListener('click', closePopup);
@@ -26,25 +26,4 @@ function saveSubmit(event) {
     profileDescription.textContent = popupDescriprion.value;
     closePopup();
 }
-popupSubmit.addEventListener('click', saveSubmit);
-
-
-
-
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
- // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+popupForm.addEventListener('submit', saveSubmit);
