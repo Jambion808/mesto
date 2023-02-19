b3f27d48e67b4b8896992904414cb8662f6ce487
Kostya Zelenin <constantine.green80@yandex.ru>
2023-02-19 17:23:22 +0300

    v8.2 style:remove redundant code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -42,23 +42,4 @@ const enableValidation = {
   errorClass: "popup__input-error_visible",
 };
 
-
-// const popupEditProfile = document.querySelector(".popup_profile");
-// const profileName = document.querySelector(".profile__name");
-// const profileDescription = document.querySelector(".profile__description");
-
-// const popupCard = document.querySelector(".popup_card");
-// const cardInputPlace = document.querySelector(".popup__input_type_place");
-// const cardInputLink = document.querySelector(".popup__input_type_url");
-// const popupProfileForm = document.forms["formPopup"];
-// const popupCardForm = document.forms["formPopup-card"];
-// const cardSubmitButton = document.querySelector(".popup__submit_type_card");
-// const popupImage = document.querySelector(".popup_image");
-// const closeButtons = document.querySelectorAll(".popup__close");
-// const popupImagePhoto = document.querySelector(".popup__image-photo");
-// const popupImageDescription = document.querySelector(
-//   ".popup__image-description"
-// );
-
-
   export {initialCards, profileEditButton, popupInputName, popupInputDescriprion, cardAddButton, cardContainer, enableValidation};
